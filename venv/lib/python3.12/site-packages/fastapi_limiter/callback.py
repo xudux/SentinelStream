d681dc4f79a1fc9a044682d06536d17929488c61
hm <xudaduxx@gmail.com>
2026-05-27 20:37:57 +0530

    Remove venv from tracking

--- venv/lib/python3.12/site-packages/fastapi_limiter/callback.py
+++
@@ -1,9 +0,0 @@
-from fastapi import HTTPException
-from starlette.status import HTTP_429_TOO_MANY_REQUESTS
-
-
-def default_callback(*args, **kwargs):
-    raise HTTPException(
-        HTTP_429_TOO_MANY_REQUESTS,
-        "Too Many Requests",
-    )
